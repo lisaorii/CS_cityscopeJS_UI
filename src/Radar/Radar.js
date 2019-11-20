62e3483d4248c78bf54fddd861756e65d8204787
Ariel Noyman <noyman@mit.edu>
2019-11-19 22:46:59 -0500

    WIP style

--- src/Radar/Radar.js
+++ src/Radar/Radar.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import { CircularGridLines, RadarChart } from "react-vis";
-import DataButton from "./dataButton";
-import "../node_modules/react-vis/dist/style.css";
+import DataButton from "../dataButton";
+import "../../node_modules/react-vis/dist/style.css";
+import "./Radar.css";
 
 const DOMAIN = [
   { name: "amazing city", domain: [0, 1] },
   { name: "validated innovation", domain: [0, 1] },
   { name: "predicted urban happiness", domain: [0, 1] },
-  { name: "mix-use from heaven", domain: [0, 1] },
+  { name: "mix-use in heaven", domain: [0, 1] },
   { name: "cool buildings", domain: [0, 1] },
   { name: "fun crime levels", domain: [0, 1] },
   { name: "success to all", domain: [0, 1] },
   { name: "happy agents", domain: [0, 1] },
   { name: "parking for all", domain: [0, 1] },
-  { name: "yes", domain: [0, 1] }
+  { name: "yes we no", domain: [0, 1] }
 ];
 
 let comparedData = {};
@@ -49,24 +50,36 @@ export default class AnimatedRadar extends Component {
             },
             axes: {
               text: {
-                opacity: 0.5
-              }
+                opacity: 0,
+                fontWeight: 500,
+                fill: "white"
+              },
+              strokeWidth: 0.1
             },
             labels: {
               textAnchor: "middle",
-              fontSize: 15
+              fontSize: 10,
+              fontWeight: "bolder",
+              fill: "white"
             }
           }}
           margin={{
-            left: 200,
-            top: 200,
-            bottom: 200,
-            right: 200
+            left: 100,
+            top: 100,
+            bottom: 100,
+            right: 100
           }}
-          width={700}
-          height={700}
+          width={500}
+          height={500}
         >
           <CircularGridLines
+            style={{
+              fill: "white",
+              fillOpacity: 0.1,
+              backgroundColor: "#fff",
+              opacity: 0.5,
+              stroke: "#333333"
+            }}
             tickValues={[...new Array(10)].map((v, i) => i / 10 - 1)}
           />
         </RadarChart>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Radar from "./Radar/Radar";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Radar />, document.getElementById("root"));
 serviceWorker.unregister();
