03f3c35624a77fcb7e0fdedae86956787e16cef4
lisaorii <42143938+lisaorii@users.noreply.github.com>
2020-01-16 13:59:42 -0500

    Update dimensions_calc_v1.py

--- dimensions_calc_v1.py
+++ dimensions_calc_v1.py
@@ -11,22 +11,23 @@ import math
 
 #variables for Density
 area_of_district = 60 #unit is square miles
-occurences = {'employed_people': 400, 'population': 1000} #set of occurences of each building type or population in a district
+occurences = {'employed_people': 400, 'population': 700} #set of occurences of each building type or population in a district
 
 #variables for Diversity
-proportions_of_species = [0.1, 0.1, 0.2, 0.2, 0.4] #proportion of the population made up of species i
+#employment_proportions = [0.1, 0.1, 0.2, 0.2, 0.4] #proportion of the population made up of types of employment buildings (co-working spaces, office buildings, etc.)
+#residential_proportions = [0.2, 0.3, 0.4] #proportion of the population made up of types of residential areas (middle class, apartments, low income class, etc.)
+proportions_dict = {'employment proportions': [0.1, 0.1, 0.2, 0.2, 0.4], 'residential proportions': [0.2, 0.3, 0.4]} 
 
 
 
 
 #Density = (absolute value of occurences) / (area of district)
-density_dict = {} #empty dictionary that stores the occurence as the key and the density as the value
+density_dict = {} #empty dictionary that stores the density of attributes
 
-for key,value in occurences.items(): #for each occurence 
-        #calculate density
-	density = float(abs(value)/area_of_district)
+for attribute,value in occurences.items(): #for each occurence in the dictionary
+	density = float(abs(value)/area_of_district) #calculate density
 	#print(key, density)
-	density_dict[key] = density #add the (occurence, density) to a dictionary
+	density_dict[key] = density #add the (occurence, density) to dictionary
 	
 print(density_dict)
 
@@ -34,12 +35,16 @@ print(density_dict)
 
 
 #Diversity = -[sum of a {proportion of a species i x log(proportion of a species i)} as i goes from 1 to total number of species]
+diversity_dict = {} #dictionary that stores the diversity of attributes
+
 diversity_total = 0
 
-for proportion in proportions_of_species:
-        diversity_total += proportion * math.log(proportion)
-        
-diversity_total = -diversity_total
+for proportions in proportions_dict: #for each item in the dictionary
+        for number in proportions_dict[proportions]: #for each value (proportion number) in each item
+                diversity_total += number * math.log(number) #calculate the diversity
+        diversity_total = -diversity_total
+        diversity_dict[proportions] = diversity_total #add (proportions, density) to dictionary
+        diversity_total = 0 #reset value
 
 print(diversity_total)
 
